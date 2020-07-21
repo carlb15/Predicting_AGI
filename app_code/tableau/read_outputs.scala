d90021fdcfe2877b1ba4b57033794893c5cfe1ff
aspen8400 <43052641+aspen8400@users.noreply.github.com>
2020-07-21 12:31:00 -0400

    Add updated files

--- app_code/tableau/read_outputs.scala
+++ app_code/tableau/read_outputs.scala
@@ -5,18 +5,18 @@ val sqlCtx = new SQLContext(sc)
 
 
 //Model summary performance
-val path1 = "hdfs:///user/mlu216/SHARE/OUTPUT/07/19/115045summary.parquet"
+val path1 = "hdfs:///user/mlu216/SHARE/FINAL/summary.parquet"
 val df1 = sqlCtx.parquetFile(path1)
 df1.show(5)
 
 //Model predictions (string county_id, float label (actual AGI), vector features, float prediction (predicted AGI), float goodness)
-val path2 = "hdfs:///user/mlu216/SHARE/OUTPUT/07/19/115045pred.parquet"
+val path2 = "hdfs:///user/mlu216/SHARE/FINAL/pred.parquet"
 val df2 = sqlCtx.parquetFile(path2)
 df2.show(5)
 
 //Training model coefficients (float coeff, string feature)
 
-val path3 = "hdfs:///user/mlu216/SHARE/OUTPUT/07/19/115045coef.parquet"
+val path3 = "hdfs:///user/mlu216/SHARE/FINAL/coef.parquet"
 val df3 = sqlCtx.parquetFile(path3)
 df3.show(5)
 
@@ -37,16 +37,25 @@ hdfs dfs -get bdad/proj/output/coefficients_table
 
 //Start of local PC code
 //Transfer files from Dumbo Storage to your PC
+
+//Predictions
 sftp ajs811@dumbo.es.its.nyu.edu:/home/ajs811/bdad_proj/data/output/predictions_2017
 //ls to see all the part files associated with that csv
 ls
 //Get each of the part files to save locally
 get part-.............csv
 
+//Model Summary
 sftp ajs811@dumbo.es.its.nyu.edu:/home/ajs811/bdad_proj/data/output/model_summary
 ls
-get
+get part-....csv
+
+//Coefficients
+sftp ajs811@dumbo.es.its.nyu.edu:/home/ajs811/bdad_proj/data/output/coefficients_table
+ls
+get part-....csv
 //End of local PC code
+
 
 //Start of Excel instructions to combine/convert results to .xlsx
 //For the model coefficients and predictions csv's, use excel to copy the two part files into one excel file (.xlsx) and add column headers shown below
